8a6ed87a1c769f3458d83cb0f4db1b192764ed14
vpidkivka <vpidkivka@eliteit.org>
2020-06-08 13:48:27 +0300

    Updates

---
+++ precache-manifest.63ff0c48cc8c2996ef8df82c5762ada4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ed91ba46e5af44eac58035d7abb769",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "032f709df53aea6d6bc2",
+    "url": "/pokedex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2920210ed8fc8c220ed9",
+    "url": "/pokedex/static/js/2.4ac1f21e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.4ac1f21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032f709df53aea6d6bc2",
+    "url": "/pokedex/static/js/main.29b87364.chunk.js"
+  },
+  {
+    "revision": "673b1b51f80d2f8e1c0f",
+    "url": "/pokedex/static/js/runtime-main.f304e274.js"
+  }
+]);
